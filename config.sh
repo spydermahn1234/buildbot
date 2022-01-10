62f7c7be84f7cfe842d9642e1b247418e50e4c2c
ArhamDharewa <89764711+ArhamDharewa@users.noreply.github.com>
2022-01-10 10:06:22 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 #
-# Orel6505
+# ZEUTS
 #
 
 ## Need To Fill
 #Sync - Requierd
-ROM_NAME=""
-ANDROID_VERSION=""
-REPO_URL=""
-REPO_BRANCH=""
+ROM_NAME="PixelExtended"
+ANDROID_VERSION="12"
+REPO_URL="https://github.com/PixelExtended/manifest"
+REPO_BRANCH="snow"
 MANIFEST_URL=""
 MANIFEST_BRANCH=""
 
 #Build - Requierd 
-DEVICE_CODENAME=""
+DEVICE_CODENAME="mojito"
 AUTO_BRINGUP=""
-LUNCH_NAME=""
-BACON_NAME=""
+LUNCH_NAME="aosp_mojito-userdebug"
+BACON_NAME="mka bacon -j16"
 
 #Upload stuff - Optional
-UPLOAD_TYPE=""
+UPLOAD_TYPE="OFF"
 UPLOAD_RECOVERY=""
-BUILD_TYPE=""
+BUILD_TYPE="OFFICIAL"
 
 #Google Drive - Optional
 GD_PATH=""
@@ -32,14 +32,14 @@ GH_USERNAME=""
 GH_REPO=""
 
 #SourceForge - Optional
-SF_USER=""
+SF_USER="zaxismxt"
 SF_PASS=""
 SF_PROJECT=""
 SF_PATH=""
 
 #Telegram - Optional
-TG_USERNAME=""
-TG_TOKEN=""
-TG_CHAT=""
+TG_USERNAME="@ZEUTS"
+TG_TOKEN="5098685564:AAF-OWxf1gjAoLUejp99AWNsdxl7DuCCAkk"
+TG_CHAT="-1001586406120"
 
 source build.sh
